7a699650c7769f559d272e6b5087c20366302ced
rolandviper <rolandviper@gmail.com>
2020-03-14 17:27:23 +0800

    added hooks for fifa and pugb rules and results

---
+++ dashboard/fifaresults.js
@@ -0,0 +1,22 @@
+function playA() {
+	nodecg.sendMessage('fifagroupshow');
+}
+
+function stopA() {
+	nodecg.sendMessage('fifagroupstop');
+}
+function playB() {
+	nodecg.sendMessage('fifaplayoffshow');
+}
+
+function stopB() {
+	nodecg.sendMessage('fifaplayoffstop');
+}
+
+function playC() {
+	nodecg.sendMessage('rulefifaplay');
+}
+
+function stopC() {
+	nodecg.sendMessage('rulefifastop');
+}
---
+++ dashboard/pubgresults.js
@@ -0,0 +1,22 @@
+function playA() {
+	nodecg.sendMessage('pubggroupshow');
+}
+
+function stopA() {
+	nodecg.sendMessage('pubggroupstop');
+}
+function playB() {
+	nodecg.sendMessage('pubgplayoffshow');
+}
+
+function stopB() {
+	nodecg.sendMessage('pubgplayoffstop');
+}
+
+function playC() {
+	nodecg.sendMessage('pubgfifaplay');
+}
+
+function stopC() {
+	nodecg.sendMessage('pubgfifastop');
+}
